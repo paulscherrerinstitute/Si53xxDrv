e1ed30c34c5ea8a5536be9b7ff39d615574588c4
till straumann <till.straumann@alumni.tu-berlin.de>
2023-06-06 15:25:58 +0200

    pdf2csv.py: added info message

--- pdf2csv.py
+++ pdf2csv.py
@@ -2,4 +2,6 @@
 import tabula
 # tables where si5395 registers are defined
 pages=[i for i in range(92,141)]
-tabula.convert_into('si5395-94-92-family.pdf','regs_raw.orig.csv',output_format='csv',pages=pages)
+ofnam='regs_raw.orig.csv'
+tabula.convert_into('si5395-94-92-family.pdf',ofnam,output_format='csv',pages=pages)
+print("Extracted table into {} -- must tweak manually (see README.fixes)".format(ofnam))
