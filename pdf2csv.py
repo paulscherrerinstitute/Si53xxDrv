f72dc27a2d5ef10c804ae411436b6aa3bed5afc0
till straumann <till.straumann@alumni.tu-berlin.de>
2023-06-13 10:50:56 +0200

    update

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import tabula
 # tables where si5395 registers are defined
-pages=[i for i in range(92,141)]
+pages=[i for i in range(92,142)]
 ofnam='regs_raw.orig.csv'
 tabula.convert_into('si5395-94-92-family.pdf',ofnam,output_format='csv',pages=pages)
 print("Extracted table into {} -- must tweak manually (see README.fixes)".format(ofnam))
--- reg_extract.py
+++ reg_extract.py
@@ -4,12 +4,19 @@ import csv
 
 # hex-number pattern
 hexp  = re.compile("^[ ]*0x[0-9a-fA-F]+$")
+# hex-number range pattern
+hrngp = re.compile("(0x[0-9a-fA-F]+)([-](0x[0-9a-fA-F]+))?")
 # bit-position range pattern
 rngp  = re.compile("([0-9]+)([:]([0-9]+))?")
 # optional hyphen plus new-line pattern
 nlp   = re.compile('[-]?[\n]')
 # Some registers (in CSV) have a index appended
 multp = re.compile('^((OPN|DESIGN)_ID)[0-9]')
+# prefix + suffix pattern
+prsup = re.compile('^([^_]+)_(([^_]*_)*)([^_]+)$')
+
+# comment
+commp = re.compile('^[ ]*[#]')
 
 class DisjointPieceError(Exception):
   def __init__(self,*args, **kwargs):
@@ -94,6 +101,12 @@ class Setting:
     return self._pcs
 
   @property
+  def piece(self):
+    if ( len(self._pcs) != 1 ):
+      raise ValueError("setting has more than one piece!")
+    return self._pcs[0]
+
+  @property
   def access(self):
     return self._acc
 
@@ -173,18 +186,110 @@ class RegSet:
             el.addPiece(Piece(addr, left, right))
           else:
             print("SKIPPING {}".format( ",".join(flds) ))
+
+    # temporary rev-index
+    rd = self.buildRevIdx()
+
+    # build 'same as' registers
+    with io.open('regs_raw.csv','r') as feil:
+      for flds in csv.reader( feil ):
+        # single register mapping (not range)
+        if len(flds) == 3 and hexp.match(flds[0]) and hexp.match(flds[2]):
+          new_name_piece = flds[1].split()[0]          
+          new_addr       = int(flds[0],0)
+          old_addr       = int(flds[2],0)
+
+          for s in rd[old_addr]:
+            new_name = self.mapName(new_name_piece, s.name)
+            p = s.piece
+            if ( len(p.addr) > 1 ):
+              raise RuntimeError("multiple addresses unexpected")
+            if not self._d.get(new_name) is None:
+              raise RuntimeError("new setting already exists")
+            ns = Setting( new_name, s.access )
+            ns.addPiece( Piece( new_addr, p.left, p.right ) )
+            self._d[new_name] = ns
+            #print("From: {:x} => {:x} ({} => {})".format(old_addr, new_addr, s.name, self.mapName(new_name_piece, s.name)))
+        elif len(flds) == 4:
+          mnew = hrngp.match(flds[0])
+          mold = hrngp.match(flds[3])
+          if ( not mnew is None and not mold is None ):
+            new_name_piece = flds[1].split()[0]          
+            oldf = int(mold.groups()[0],0)
+            newf = int(mnew.groups()[0],0)
+            go   = mold.groups()[2]
+            gn   = mnew.groups()[2]
+            if (go is None) != (gn is None):
+              raise RuntimeError("range mapping: unrecognized old->new address mapping")
+            if ( go is None ):
+              oldt = oldf
+              newt = newf
+            else:
+              oldt = int(go, 0)
+              newt = int(gn, 0)
+            if ( newt-newf != oldt-oldf ):
+              raise RuntimeError("new settings: size of new range differs from old")
+            s    = rd[oldf]
+            if ( len(s) > 1 ):
+              raise RuntimeError("old address mapped to multiple settings (not supported ATM)")
+            else:
+              s = s[0]
+            ns = Setting( self.mapName( new_name_piece, s.name ), s.access )
+            #print("From: {:x}-{:x} => {:x} ({} => {})".format(oldf, oldt, newf, s.name, self.mapName(new_name_piece, s.name) ))
+            # assume addresses are increasing
+            p = s.piece
+            l = len(p.addr)
+            if ( l != oldt - oldf + 1 ):
+              raise RuntimeError("unexpected old address range")
+            for i in range(l):
+              if ( p.addr[i] != oldf + i ):
+                raise RuntimeError("unexpected old address range ordering")
+            r = p.right
+            a = newf
+            while ( r <= p.left ):
+              l = r | 7
+              if ( l > p.left ):
+                l = p.left
+              ns.addPiece( Piece( a, l, r ) )
+              r  = l + 1
+              a += 1
+            if ( self._d.get( ns.name ) ):
+              raise KeyError("newly mapped name already exists")
+            self._d[ns.name] = ns
+
+    # build complete reverse index
+    self._rd = self.buildRevIdx()
+
+  def find(self, key):
+    if isinstance(key, int):
+      return self._rd[key]
+    else:
+      return self._d[key]
+
+  @staticmethod
+  def mapName(new_name_piece, old_name):
+    # heuristics: if new_name_piece already contains a '_' we assume it is the complete name
+    if ( prsup.match( new_name_piece ) ):
+      return new_name_piece
+    if ( prsup.match( old_name ) ):
+      return prsup.sub(new_name_piece + "_" + '\\2\\4', old_name)
+    # old name has no '_'; new piece is the replacement
+    return new_name_piece
+
+  def buildRevIdx(self):
     # build reverse index
-    self._rd = dict()
+    rd = dict()
     for k,v in self._d.items():
       for p in v.pieces:
         for a in p.addr:
-          settings = self._rd.get(a)
+          settings = rd.get(a)
           if settings is None:
             settings = []
-            self._rd[a] = settings
+            rd[a] = settings
           if v in settings:
             raise RuntimeError("Index consistency check: same setting found multiple times for single address")
           settings.append(v)
+    return rd
 
   def consistencyCheck(self):
     flag = 0
@@ -219,6 +324,24 @@ class RegSet:
                 print("Register @0x{:x} shared between {} and {}".format( a, s.name, v.name ))
                 flag += 1
     return flag
-    
+
+  # annotate a CBP register file
+  def annotateCBP(self, fnam):
+    pat = re.compile("^[ ]*(0x[0-9a-fA-F]+)[ ]*[,][ ]*(0x[0-9a-fA-F]*)")
+    with io.open(fnam, 'r') as f: 
+      for l in f:
+        m = pat.match(l)
+        print(l[:-1], end = '')
+        if (not m is None):
+          print('# ', end='')
+          a = int(m.groups()[0],0)
+          v = int(m.groups()[1],0)
+          try:
+            for s in self.find(a):
+              print(" {}".format( s.name ), end='')
+          except KeyError:
+            print(" *UNDOCUMENTED*", end='')
+        print()
+
 rs=RegSet()
 rs.consistencyCheck()
